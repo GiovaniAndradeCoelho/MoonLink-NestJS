9ab905c1faa224f8435a348038dbc976d4c14e51
GiovaniAndrade <giovani.andrade@quantumbyte.com.br>
2025-02-20 13:20:58 -0300

    feat(middleware): allow access to auth/* and server/* routes

---
+++ src/app.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AppMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+
+    const validToken = process.env.API_SECRET_TOKEN;
+    let tokenFromRequest = req.headers['authorization'];
+
+    if (!tokenFromRequest) {
+      return res.status(401).json({ message: 'Token não fornecido.' });
+    }
+
+    if (typeof tokenFromRequest === 'string' && tokenFromRequest.startsWith('Bearer ')) {
+      tokenFromRequest = tokenFromRequest.replace('Bearer ', '');
+    }
+
+    if (tokenFromRequest !== validToken) {
+      return res.status(401).json({ message: 'Token inválido.' });
+    }
+
+    next();
+  }
+}
